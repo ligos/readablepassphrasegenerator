06dd342298b587a940e9a7cb8e3a9334c874ab8f
ligos <none@none>
2013-05-02 20:50:49 +1000

    Update console version help for new phrase strengths.

--- trunk/Generator/Program.cs
+++ trunk/Generator/Program.cs
@@ -288,7 +288,7 @@ namespace MurrayGrant.ReadablePassphrase.Generator
             Console.WriteLine("  -c --count nnn        Generates nnn phrases (default: {0})", count);
             Console.WriteLine("  -s --strength xxx     Selects phrase strength (default: {0})", strength);
             Console.WriteLine("                xxx =     [normal|strong|insane][equal|required][and|speech]");
-            Console.WriteLine("                          or 'custom' or 'random'");
+            Console.WriteLine("                          or 'custom' or 'random[short|long|forever]'");
             Console.WriteLine("  --spaces true|false   Includes spaces between words (default: {0})", includeSpaces);
             Console.WriteLine("  -l --loaderdll path   Specifies a custom loader dll");
             Console.WriteLine("  -t --loadertype path  Specifies a custom loader type");
