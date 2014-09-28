6e8db02a31d0b343ba206fc3643a11a73e5c68ef
ligos <none@none>
2014-09-28 21:03:28 +1000

    Remove config form test.

--- trunk/Test/Program.cs
+++ trunk/Test/Program.cs
@@ -120,7 +120,7 @@ namespace Test
             // TODO: Test load an alternate dictionary loader.
 
             // Test the config form.
-            TestConfigForm(generator);
+            //TestConfigForm(generator);
 
             // Test mutators.
             //GenerateMutatedSamples(PhraseStrength.Random, generator, 10, new IMutator[] { new UppercaseRunMutator() });
