6806cd07930daf0f87266ec4a3466517a1781c64
ligos <none@none>
2015-10-16 18:06:25 +1100

    Bump version numbers and copyright year for release.

--- trunk/Generator/Properties/AssemblyInfo.cs
+++ trunk/Generator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Murray Grant")]
 [assembly: AssemblyProduct("Readable Passphrase Generator")]
-[assembly: AssemblyCopyright("Copyright © Murray Grant 2011-2013")]
+[assembly: AssemblyCopyright("Copyright © Murray Grant 2011-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.14.1.0")]
-[assembly: AssemblyFileVersion("0.14.1.0")]
+[assembly: AssemblyVersion("0.15.0.0")]
+[assembly: AssemblyFileVersion("0.15.0.0")]
