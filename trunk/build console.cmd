28d189544687b2911f5734d343588618e3d068aa
ligos <none@none>
2018-07-21 17:15:58 +1000

    Fix console build process.

--- trunk/build console.cmd
+++ trunk/build console.cmd
@@ -7,10 +7,12 @@ del /q PassphraseGenerator.net452.zip
 zip.exe -9j PassphraseGenerator.net452.zip PassphraseGenerator\bin\Release\net452\*.* 
 zip.exe -u PassphraseGenerator.net452.zip LICENSE.txt NOTICE.txt README.txt
 
+rem Publish for .NET Core 2.1
+dotnet publish PassphraseGenerator -c Release -f netcoreapp2.1 
+
 rem Build ZIP file for .NET Core 2.1
 del /q PassphraseGenerator.netcoreapp21.zip
-zip.exe -9j PassphraseGenerator.netcoreapp21.zip PassphraseGenerator\bin\Release\netcoreapp2.1\*.* 
+zip.exe -9j PassphraseGenerator.netcoreapp21.zip PassphraseGenerator\bin\Release\netcoreapp2.1\publish\*.* 
 zip.exe -u PassphraseGenerator.netcoreapp21.zip LICENSE.txt NOTICE.txt
-zip.exe -d PassphraseGenerator.netcoreapp21.zip PassphraseGenerator.runtimeconfig.dev.json
 
 pause
