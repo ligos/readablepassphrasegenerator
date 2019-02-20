447b004124433cd568d5da2da01392936b51450f
ligos <none@none>
2019-02-20 18:30:45 +1100

    Change project to allow references to nugets and projects, to make debugging easier.

--- trunk/build nuget.cmd
+++ trunk/build nuget.cmd
@@ -2,9 +2,9 @@ rem Rebuild
 dotnet clean -c Release
 
 rem Build each package.
-dotnet pack ReadablePassphrase.Words -c Release --include-symbols -o ../../releases
-dotnet pack ReadablePassphrase.Core -c Release --include-symbols -o ../../releases
-dotnet pack ReadablePassphrase.DefaultDictionary -c Release --include-symbols -o ../../releases
-dotnet pack ReadablePassphrase -c Release --include-symbols -o ../../releases
+dotnet pack ReadablePassphrase.Words -c Release --include-symbols /p:RefNugets=True -o ../../releases
+dotnet pack ReadablePassphrase.Core -c Release --include-symbols /p:RefNugets=True -o ../../releases
+dotnet pack ReadablePassphrase.DefaultDictionary -c Release --include-symbols /p:RefNugets=True -o ../../releases
+dotnet pack ReadablePassphrase -c Release --include-symbols /p:RefNugets=True -o ../../releases
 
 pause
