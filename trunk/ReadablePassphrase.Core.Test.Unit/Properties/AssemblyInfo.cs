59d4696625dbba2712920d1138d1c910c8baf8b3
Andrew Brown <andy@andygbrown.co.uk>
2020-05-24 17:05:14 +0100

    Make unit tests run in parallel

---
+++ trunk/ReadablePassphrase.Core.Test.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("82725C88-681F-4953-9040-0959F90D882C")]
+
+[assembly: Parallelizable(ParallelScope.Children)]
