2e850a7dfa110e34770070d8e83293fed0265244
Murray Grant <github@ligos.net>
2025-11-30 15:10:04 +1100

    Update build script for net10.0

--- trunk/build console.cmd
+++ trunk/build console.cmd
@@ -10,16 +10,6 @@ copy /y README.txt PassphraseGenerator\bin\Release\net452\README.txt
 zip.exe -9j PassphraseGenerator.net452.zip PassphraseGenerator\bin\Release\net452\*.* 
 
 
-rem Publish for .NET 6.0
-dotnet publish PassphraseGenerator -c Release -f net60
-
-rem Build ZIP file for .NET 6.0
-del /q PassphraseGenerator.net60.zip
-copy /y ..\LICENSE.txt PassphraseGenerator\bin\Release\net60\publish\LICENSE.txt
-copy /y ..\NOTICE.txt PassphraseGenerator\bin\Release\net60\publish\NOTICE.txt
-copy /y README.txt PassphraseGenerator\bin\Release\net60\publish\README.txt
-zip.exe -9j PassphraseGenerator.net60.zip PassphraseGenerator\bin\Release\net60\publish\*.* 
-
 rem Publish for .NET 8.0
 dotnet publish PassphraseGenerator -c Release -f net8.0
 
@@ -29,6 +19,16 @@ copy /y ..\LICENSE.txt PassphraseGenerator\bin\Release\net8.0\publish\LICENSE.tx
 copy /y ..\NOTICE.txt PassphraseGenerator\bin\Release\net8.0\publish\NOTICE.txt
 copy /y README.txt PassphraseGenerator\bin\Release\net8.0\publish\README.txt
 zip.exe -9j PassphraseGenerator.net8.0.zip PassphraseGenerator\bin\Release\net8.0\publish\*.* 
+
+rem Publish for .NET 10.0
+dotnet publish PassphraseGenerator -c Release -f net10.0
+
+rem Build ZIP file for .NET 10.0
+del /q PassphraseGenerator.net10.0.zip
+copy /y ..\LICENSE.txt PassphraseGenerator\bin\Release\net10.0\publish\LICENSE.txt
+copy /y ..\NOTICE.txt PassphraseGenerator\bin\Release\net10.0\publish\NOTICE.txt
+copy /y README.txt PassphraseGenerator\bin\Release\net10.0\publish\README.txt
+zip.exe -9j PassphraseGenerator.net10.0.zip PassphraseGenerator\bin\Release\net10.0\publish\*.* 
 
 
 pause
